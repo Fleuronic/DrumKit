6c8ab279d6791b7993622dd7f3e4959f74689723
Jordan Kay <jordan@fleuronic.com>
2025-10-28 17:48:40 -0400

    Update corps

--- Sources/DrumKit/Corps/Corps.swift
+++ Sources/DrumKit/Corps/Corps.swift
@@ -1,8 +1,11 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
+import Foundation
 import MemberwiseInit
 
 @MemberwiseInit(.public)
 public struct Corps: Equatable, Sendable {
 	public let name: String
+	public let url: URL?
+	public let isActive: Bool
 }
