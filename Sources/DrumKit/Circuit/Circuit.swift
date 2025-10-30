05124ecdf9beaae6801a93de226c8028b88007c0
Jordan Kay <jordan@fleuronic.com>
2025-10-30 12:20:00 -0400

    Update circuit

--- Sources/DrumKit/Circuit/Circuit.swift
+++ Sources/DrumKit/Circuit/Circuit.swift
@@ -1,9 +1,11 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
+import Foundation
 import MemberwiseInit
 
 @MemberwiseInit(.public)
 public struct Circuit: Equatable, Sendable {
 	public let name: String
 	public let abbreviation: String?
+	public let url: URL?
 }
