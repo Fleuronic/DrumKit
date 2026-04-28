d40f4f8943796d89710b283ec623adc917da5230
Jordan Kay <jordan@fleuronic.com>
2026-04-28 19:34:54 -0400

    Update time

--- Sources/DrumKit/Event/Time.swift
+++ Sources/DrumKit/Event/Time.swift
@@ -4,9 +4,16 @@ import Foundation
 import MemberwiseInit
 
 @MemberwiseInit(.public)
-public struct Time: Equatable, Sendable {
+public struct Time: Sendable {
 	public let offset: TimeInterval
 	public let zone: TimeZone
+}
+
+// MARK: -
+extension Time: Equatable {
+	public static func ==(lhs: Time, rhs: Time) -> Bool {
+		lhs.offset == rhs.offset
+	}
 }
 
 // MARK: -
